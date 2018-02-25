ac99aee326c65ae333387877d5d9ef2d60b7654f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-02-25 19:58:37 +0000

    example code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-console.log("hello")
-foo.innerHTML = "Hello from Webpack 4!";
+console.log("Hello from Webpack 4")
+app.innerHTML = "Hello from Webpack 4! This is " + (process.env.NODE_ENV || "unknown") + " build.";
